a8f1783763e7cf2171483412709f109228e4725b
MarcyL <supralai@gmail.com>
2016-05-23 22:39:24 +0800

    add function for show the user information in page

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    © Copyright 2015-2016 Arcade Crafts 遊戲工作室. All Rights Reserved.
+                    © Copyright 2015-2016 Arcade Crafts 遊戲工作室. All Rights Reserved
                 </div>
                 <div class="col-md-5 col-md-offset-1">
                     <a style="font-size:14px" href="{{ URL::asset('/aboutUs') }}">《關於我們》</a> / <a href="{{ URL::asset('/aboutUs#tabus2') }}" style="font-size:14px">《服務條款》</a> / <a href="{{ URL::asset('/aboutUs#tabus4') }}" style="font-size:14px">《私隱政策》</a>
