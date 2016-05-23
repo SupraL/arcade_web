a1ec5f58b3c56c705bf450b20ceb7b18c7105676
MarcyL <supralai@gmail.com>
2016-05-23 22:38:10 +0800

    add function for show the user information in page

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,13 @@
 <footer class="page-footer stylish-color" if="footer">
     <div class="footer-copyright">
         <div class="container">
-            <div class="col-md-10 col-md-offset-1"> © 2015 Copyright <a href="http://www.MDBootstrap.com">arcadecrafts.net </a>
+            <div class="row">
+                <div class="col-md-6">
+                    © Copyright 2015-2016 Arcade Crafts 遊戲工作室. All Rights Reserved.
+                </div>
+                <div class="col-md-5 col-md-offset-1">
+                    <a style="font-size:14px" href="{{ URL::asset('/aboutUs') }}">《關於我們》</a> / <a href="{{ URL::asset('/aboutUs#tabus2') }}" style="font-size:14px">《服務條款》</a> / <a href="{{ URL::asset('/aboutUs#tabus4') }}" style="font-size:14px">《私隱政策》</a>
+                </div>
             </div>
         </div>
     </div>
