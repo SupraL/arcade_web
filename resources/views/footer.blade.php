1ae69bff74d2c5e48e941f21a5e99834192c5747
kelvinahke <kelvinahke@gmail.com>
2016-05-23 21:57:32 +0800

    index footer small edit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="page-footer stylish-color" if="footer">
     <div class="footer-copyright">
         <div class="container">
-            <div class="col-md-10 col-md-offset-1"> © 2015 Copyright <a href="http://www.MDBootstrap.com">arcadecrafts.net </a>
+            <div class="col-md-10 col-md-offset-1"> © Copyright 2015-2016 Arcade Crafts 遊戲工作室. All Rights Reserved.<br />
+                <a href="aboutUs">《關於我們》</a> / <a href="aboutUs#tabus2">《服務條款》</a> / <a href="aboutUs#tabus3">《服務政策》</a>
             </div>
         </div>
     </div>
