661b69b69a53233caf387c1db886bb2c94264077
kelvinahke <kelvinahke@gmail.com>
2016-05-23 22:08:55 +0800

    index footer small edit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="page-footer stylish-color" if="footer">
     <div class="footer-copyright">
         <div class="container">
-            <div class="col-md-10 col-md-offset-1"> © Copyright 2015-2016 Arcade Crafts 遊戲工作室. All Rights Reserved.<br />
-                <a href="aboutUs">《關於我們》</a> / <a href="aboutUs#tabus2">《服務條款》</a> / <a href="aboutUs#tabus3">《服務政策》</a>
+            <div class="col-md-10 col-md-offset-1"> © Copyright 2015-2016 Arcade Crafts 遊戲工作室. All Rights Reserved.<br>
+                <a href="aboutUs">《關於我們》</a> / <a href="aboutUs#tabus2">《服務條款》</a> / <a href="aboutUs#tabus4">《私隱政策》</a>
             </div>
         </div>
     </div>
