42708f4ac33295b71320484fac2aa84aadb63cdb
MarcyL <supralai@gmail.com>
2016-05-25 01:05:31 +0800

    add function for show the user information in page

--- app/Http/Middleware/ForceHttpProtocol.php
+++ app/Http/Middleware/ForceHttpProtocol.php
@@ -2,13 +2,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
 
 class ForceHttpProtocol {
     public function handle($request, Closure $next) {
-        if (!$request->secure()) {
-            return redirect()->secure($request->path());
+        //!$request->secure()
+        $domain = $_SERVER['SERVER_NAME'];
+        if($domain == "localhost"){
+            if (!$request->secure()) {
+                return redirect()->secure($request->path());
+            }
+        }else{
+            if ($request->header('x-forwarded-proto') <> 'https') {
+                return redirect()->secure($request->path());
+            }
         }
+
         return $next($request);
     }
 }
