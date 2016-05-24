d9a5df2e1c5ea5c19c2327bc55ff12a906e9e572
MarcyL <supralai@gmail.com>
2016-05-25 01:19:54 +0800

    add function for show the user information in page

--- resources/views/loginSuccess.blade.php
+++ resources/views/loginSuccess.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/mdb.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/custom_style.css') }}">
-    <script src="{{ URL::asset('js/jquery.min.js')}}"></script>
-    <script src="{{ URL::asset('js/bootstrap.min.js')}}"></script>
-    <script src="{{ URL::asset('js/mdb.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/mdb.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/custom_style.css') }}">
+    <script src="{{ secure_asset('js/jquery.min.js')}}"></script>
+    <script src="{{ secure_asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{ secure_asset('js/mdb.js')}}"></script>
     <meta charset="UTF-8">
     <title>ArcadeCrafts</title>
 </head>
